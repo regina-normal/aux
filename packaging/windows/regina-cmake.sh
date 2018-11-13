9305d1fdeb30cad8121dd2c079159e2676577f0a
Benjamin Burton <bab@maths.uq.edu.au>
2018-11-14 08:46:12 +1000

    Qt version bump.

--- packaging/windows/regina-cmake.sh
+++ packaging/windows/regina-cmake.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/home/bab/software/lib/pkgconfig:/c/Program Files/Graphviz2.38/lib/pkgconfig"
 cmake -G 'MSYS Makefiles' -DQDBM=1 \
-  -DCMAKE_PREFIX_PATH=/c/Qt/Qt5.10.1/5.10.1/mingw53_32 \
+  -DCMAKE_PREFIX_PATH=/c/Qt/Qt5.11.2/5.11.2/mingw53_32 \
   -DCMAKE_INSTALL_PREFIX=/home/bab/software \
   -DREGINA_BOOST_DO_NOT_FIX_CONVERTERS=1 \
   ..
