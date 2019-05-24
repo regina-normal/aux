a4d0a7e5aa67bfd72fed843e96b6d66b99fab5cd
Benjamin Burton <bab@maths.uq.edu.au>
2019-05-24 15:08:24 +1000

    Allow variables in Regina.wxs.template.

--- packaging/windows/mkwxs.pl
+++ packaging/windows/mkwxs.pl
@@ -1,8 +1,29 @@
 #!/usr/bin/perl -w
 use strict;
 
+my $qtver = '5.12.3';
+my $mingwver = '73';
+
 open(TEMPLATE, '<', 'Regina.wxs.template') or die;
 open(WXS, '>', 'Regina.wxs') or die;
+
+my $msys;
+my $mingw;
+my $qt;
+
+my $arch = `uname -m`;
+chomp $arch;
+if ($arch eq 'x86_64') {
+    $msys = 'c:\msys64';
+    $mingw = 'c:\msys64\mingw64';
+    $qt = "c:\\Qt\\$qtver\\mingw${mingwver}_64";
+} elsif ($arch eq 'i686') {
+    $msys = 'c:\msys32';
+    $mingw = 'c:\msys32\mingw32';
+    $qt = "c:\\Qt\\$qtver\\mingw${mingwver}_32";
+} else {
+    die "Unknown architecture: $arch";
+}
 
 my $line;
 my $partial = '';
@@ -12,6 +33,10 @@ my $currPath = undef;
 my $currExclude = {};
 my ($name, $prefix, $suffix);
 while (<TEMPLATE>) {
+    s/\$msys/$msys/g;
+    s/\$mingw/$mingw/g;
+    s/\$qt/$qt/g;
+
     />.*</ and die;
 
     if (not ($partial or /</)) {
