a61cd55867d83e7a65b6fef220d84bd4256a70d2
Benjamin Burton <bab@maths.uq.edu.au>
2019-08-30 11:44:49 +0200

    Debian now requires ascii armour in repo signatures.

--- packaging/debian/sign-remote.sh
+++ packaging/debian/sign-remote.sh
@@ -28,7 +28,7 @@ for suite in $suites; do
   echo "I: retrieving file to sign from remote host ..."
   scp "$host:$path" $tmp
   echo "I: signing ..."
-  gpg --default-key 0x542D38D9 --digest-algo SHA256 --detach-sign -o $sig $tmp
+  gpg --default-key 0x542D38D9 --digest-algo SHA256 --detach-sign -a -o $sig $tmp
   echo "I: sending back signature ..."
   scp $sig "$host":"$path.gpg"
   echo "I: remote signing done."
