13be7fd44b641dcf870f6a210987057339486a16
Benjamin Burton <bab@maths.uq.edu.au>
2021-03-17 11:56:15 +1000

    Remove the last vestiges of the hard-coded username bab from windows packaging.

--- packaging/windows/regina-cmake.sh
+++ packaging/windows/regina-cmake.sh
@@ -15,14 +15,14 @@ else
 fi
 
 if [ -z "$gvdir" ]; then
-  export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/home/bab/software/lib/pkgconfig"
+  export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:~/software/lib/pkgconfig"
 else
-  export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/home/bab/software/lib/pkgconfig:$gvdir/lib/pkgconfig"
+  export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:~/software/lib/pkgconfig:$gvdir/lib/pkgconfig"
 fi
 
 cmake -G 'MSYS Makefiles' -DQDBM=1 \
   -DCMAKE_PREFIX_PATH="$qtdir" \
-  -DCMAKE_INSTALL_PREFIX=/home/bab/software \
+  -DCMAKE_INSTALL_PREFIX="$HOME"/software \
   -DPYTHON_CORE_IN_ZIP=1 \
   ..
 
