4070fef0eae6ea9e228a332a79c6d35b768df841
Benjamin Burton <bab@maths.uq.edu.au>
2023-11-30 13:49:38 +1000

    Tweak for older suse.

--- packaging/docker/rinse/opensuse-core.pl
+++ packaging/docker/rinse/opensuse-core.pl
@@ -26,7 +26,7 @@ while ($#latest >= 0) {
         my $info = `zypper --no-refresh info --requires '$target'`;
         if ($info =~ /^.*Requires\s*:\s*---\s*$/s) {
             next;
-        } elsif ($info =~ /^.*Requires\s*:\s*\[\d+\]\n(.*)$/s) {
+        } elsif ($info =~ /^.*Requires\s*:\s*\[\d+\] *\n(.*)$/s) {
             $info = $1;
         } elsif ($info =~ /^.*Requires\s*:\s*([a-zA-Z0-9()\/._<>= -]+\n)\s*$/s) {
             $info = $1;
