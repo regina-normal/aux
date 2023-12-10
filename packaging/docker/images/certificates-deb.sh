c683106f1cfb160dd63b4d3864e2976fb2e5108e
Benjamin Burton <bab@maths.uq.edu.au>
2023-12-11 07:42:12 +1000

    Edit the source repos for the new certificates.

--- packaging/docker/images/certificates-deb.sh
+++ packaging/docker/images/certificates-deb.sh
@@ -9,12 +9,30 @@ fi
 
 echo "Adding certificates from $certsuite..."
 
-cat > /etc/apt/sources.list.d/cert.list <<__END__
-deb http://old-releases.ubuntu.com/ubuntu $certsuite-security main
-deb-src http://old-releases.ubuntu.com/ubuntu $certsuite-security main
-__END__
+certlist=/etc/apt/sources.list.d/cert.list
+certprefs=/etc/apt/preferences.d/cert.prefs
 
-cat > /etc/apt/preferences.d/cert.prefs <<__END__
+arch=`uname -m`
+
+# Add an extra APT source for $certsuite-security.
+# Where this comes from (e.g., the main ubuntu servers / ports / etc.) depends
+# on the underlying architecture.  We assume for the moment that the suite is
+# still current (i.e., it lives on the main ubuntu archive/ports servers and
+# not old-releases).
+case "$arch" in
+  aarch64 )
+    cat > $certlist <<__END__
+deb http://ports.ubuntu.com/ubuntu-ports $certsuite-security main
+__END__
+    ;;
+  * )
+    cat > $certlist <<__END__
+deb http://archive.ubuntu.com/ubuntu $certsuite-security main
+__END__
+    ;;
+esac
+
+cat > $certprefs <<__END__
 Package: *
 Pin: release a=$certsuite-security
 Pin-Priority: 100
@@ -30,5 +48,4 @@ apt-get install ca-certificates
 
 # Remove the new sources, so we don't accidentally drag anything else in that
 # we didn't want.
-rm /etc/apt/sources.list.d/cert.list
-rm /etc/apt/preferences.d/cert.prefs
+rm $certlist $certprefs
