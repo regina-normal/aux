605c1efde1f4987b8b0318d6485afe5036d2f18e
Benjamin Burton <bab@maths.uq.edu.au>
2024-01-11 22:08:51 +1000

    More progress on VNC.

---
+++ packaging/docker/images/startvnc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+if [ -e /usr/bin/vncserver ]; then
+  cmd=/usr/bin/vncserver
+elif [ -e /usr/bin/X11/vncserver ]; then
+  cmd=/usr/bin/X11/vncserver
+else
+  echo 'ERROR: Cannot locate vncserver executable'
+  exit 1
+fi
+
+"$cmd" :1 -geometry 1280x1024 -rfbport 5901
+sleep infinity
